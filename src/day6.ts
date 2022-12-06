81b74837385c9902f5c80950f76af6a1ca550d79
machineshon <mkzk.mail@gmail.com>
2022-12-06 16:51:36 -0300

    gotta go shorter

--- src/day6.ts
+++ src/day6.ts
@@ -1,34 +1,27 @@
 import { readStatic } from "./utils"
 
-// finds the index where the last [size] chars in [chars] are all different
-const findHeteroslice = (chars: string[], size: number) => {
-    let point = -1
-    chars.forEach((_, index) => {
-        const start = index - size
-        const end = index
-        // don't need slices that are too small
-        if (start < 0) return
-
-        const codes = chars.slice(start, end).map(char => char.charCodeAt(0))
-        const isDuplicate = codes.some(
-            code => codes.indexOf(code) != codes.lastIndexOf(code)
-        )
-
-        if (!isDuplicate && point == -1) {
-            point = index
-        }
-    })
-    return point
-}
+const findHeteroslice = (chars: string[], size: number): number =>
+    chars.reduce(
+        (x, _, index) =>
+            index - size > 0 &&
+            !chars
+                .slice(index - size, index)
+                .map(char => char.charCodeAt(0))
+                .some(
+                    (code, _, codes) =>
+                        codes.indexOf(code) != codes.lastIndexOf(code)
+                ) &&
+            x == -1
+                ? index
+                : x,
+        -1
+    )
 
 class Packet {
     public static from(s: string): Packet {
         const chars = s.split("").filter(char => char != "\n")
 
-        let markStartAt = findHeteroslice(chars, 4)
-        let messageMarkAt = findHeteroslice(chars, 14)
-
-        return new Packet(markStartAt, messageMarkAt)
+        return new Packet(findHeteroslice(chars, 4), findHeteroslice(chars, 14))
     }
 
     constructor(public sopMarkerIdx: number, public messageMarkerIdx: number) {}

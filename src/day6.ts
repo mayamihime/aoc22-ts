d23d6e3890cf89c51892c041cf605e429e49911b
machineshon <mkzk.mail@gmail.com>
2022-12-06 15:25:26 -0300

    day 6 refatoring

--- src/day6.ts
+++ src/day6.ts
@@ -1,54 +1,43 @@
 import { readStatic } from "./utils"
 
+// finds the index where the last [size] chars in [chars] are all different
+const findHeteroslice = (chars: string[], size: number) => {
+    let point = -1
+    chars.forEach((_, index) => {
+        const start = index - size 
+        const end = index
+        // don't need slices that are too small
+        if (start < 0) return;
+
+        const codes = chars.slice(start, end)
+            .map(char => char.charCodeAt(0))
+        const isDuplicate = codes.some(code => codes.indexOf(code) != codes.lastIndexOf(code)) 
+
+        if(!isDuplicate && point == -1) {
+            point = index
+        }
+    })
+    return point
+}
+
 class Packet {
     public static from(s: string): Packet {
         const chars = s.split("")
             .filter(char => char != "\n")
         
-        let markStartAt = -1
-        chars.forEach((_, index) => {
-            const start = index - 4
-            const end = index
-
-            if (start < 0) return;
-
-            const codes = chars.slice(start, end)
-                .map(_char => _char.charCodeAt(0))
-            const isDuplicate = codes.some(code => codes.indexOf(code) != codes.lastIndexOf(code)) 
-
-            if(!isDuplicate && markStartAt == -1) {
-                markStartAt = index
-            }
-        })
-
-        let messageMarkAt = -1
-
-        console.log(chars)
-        chars.forEach((_, index) => {
-            const start = index - 14
-            const end = index
-
-            if (start < 0) return;
-            
-            const codes = chars.slice(start, end)
-                .map(_char => _char.charCodeAt(0))
-            const isDuplicate = codes.some(code => codes.indexOf(code) != codes.lastIndexOf(code))
-
-            if(!isDuplicate && messageMarkAt == -1) {
-                messageMarkAt = index
-            }
-        })
+        let markStartAt = findHeteroslice(chars, 4)
+        let messageMarkAt = findHeteroslice(chars, 14)
 
         return new Packet(markStartAt, messageMarkAt)
     }
 
-    constructor(public markStarsAt: number, public messageMarkAt: number) {}
+    constructor(public sopMarkerIdx: number, public messageMarkerIdx: number) {}
 }
 
 ;(async () => {
     const contents = await readStatic("day6_input.prod")
     
     const packet = Packet.from(contents)
-    console.log(`the marker starts at ${packet.markStarsAt}`)
-    console.log(`the message marker starts at ${packet.messageMarkAt}`)
+    console.log(`the start-of-packet marker is at ${packet.sopMarkerIdx}`)
+    console.log(`the message marker is at ${packet.messageMarkerIdx}`)
 })()
